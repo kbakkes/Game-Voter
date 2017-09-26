4bab552486e2edc20f614c3c34149478bef64457
kbakkes <karim.bakkes@gmail.com>
2017-09-26 16:07:01 +0200

    Added entities

---
+++ src/AppBundle/Entity/Game.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Game
+ *
+ * @ORM\Table(name="game")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GameRepository")
+ */
+class Game
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="year", type="string", length=255)
+     */
+    private $year;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="genre_id", type="integer")
+     */
+    private $genreId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cover", type="string", length=255)
+     */
+    private $cover;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="uploaded_by_id", type="integer")
+     */
+    private $uploadedById;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="upvotes", type="integer")
+     */
+    private $upvotes;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Game
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set year
+     *
+     * @param string $year
+     *
+     * @return Game
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Get year
+     *
+     * @return string
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * Set genreId
+     *
+     * @param integer $genreId
+     *
+     * @return Game
+     */
+    public function setGenreId($genreId)
+    {
+        $this->genreId = $genreId;
+
+        return $this;
+    }
+
+    /**
+     * Get genreId
+     *
+     * @return int
+     */
+    public function getGenreId()
+    {
+        return $this->genreId;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Game
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set cover
+     *
+     * @param string $cover
+     *
+     * @return Game
+     */
+    public function setCover($cover)
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    /**
+     * Get cover
+     *
+     * @return string
+     */
+    public function getCover()
+    {
+        return $this->cover;
+    }
+
+    /**
+     * Set uploadedById
+     *
+     * @param integer $uploadedById
+     *
+     * @return Game
+     */
+    public function setUploadedById($uploadedById)
+    {
+        $this->uploadedById = $uploadedById;
+
+        return $this;
+    }
+
+    /**
+     * Get uploadedById
+     *
+     * @return int
+     */
+    public function getUploadedById()
+    {
+        return $this->uploadedById;
+    }
+
+    /**
+     * Set upvotes
+     *
+     * @param integer $upvotes
+     *
+     * @return Game
+     */
+    public function setUpvotes($upvotes)
+    {
+        $this->upvotes = $upvotes;
+
+        return $this;
+    }
+
+    /**
+     * Get upvotes
+     *
+     * @return int
+     */
+    public function getUpvotes()
+    {
+        return $this->upvotes;
+    }
+}
+
